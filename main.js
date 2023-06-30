707a13f17c84e6dae3e44ca992c4e240a4811ac9
istree <istree@gmail.com>
2023-06-30 14:42:35 +0800

    style: update space

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const port = 8080
 
 
 app.get('/', async (req, res) => {
-	const resData = await fetch('https://api64.ipify.org?format=json').then(res => res.json());
-	console.log('resData:', resData)
+    const resData = await fetch('https://api64.ipify.org?format=json').then(res => res.json());
+    console.log('resData:', resData)
 
-	res.send(resData)
+    res.send(resData)
 })
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
